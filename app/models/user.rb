e861075d500f06c3c23aab87d8ac90816c33d1a3
rakm <rakm@rakm.io>
2013-01-26 20:56:01 -0800

    Add user signups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  attr_accessible :email, :full_name, :password, :password_confirmation
+  attr_accessor :password
+
+  validates_confirmation_of :password
+  validates_presence_of :full_name
+  validates_presence_of :password, on: :create
   validates_presence_of :email
+  validates_uniqueness_of :email
+  
+  before_save :encrypt_password
 
   has_many :memberships
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
   end
 end
