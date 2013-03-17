429e1a0ebd4c1bb2c62268a590ebb989ae906a12
rakm <rakm@rakm.io>
2013-03-16 20:07:44 -0700

    Users and visitors can vote on bulletins now

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,5 +1,6 @@
 class Bulletin < ActiveRecord::Base
   attr_accessible :body, :title, :url, :bulletin_type
+  has_many :votes, :as => :votable
 
   def self.find_by_title(title)
     Bulletin.where("lower(title) = lower(:title)", :title => title)
