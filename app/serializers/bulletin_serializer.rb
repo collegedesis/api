84f2f0c10522f0d790068b31f1a5182f78b7786d
mehulkar <mehul.kar@gmail.com>
2013-03-28 06:09:00 -0700

    Ability to recommend a bulletin while reading it. Also displays number of total recommendations

--- app/serializers/bulletin_serializer.rb
+++ app/serializers/bulletin_serializer.rb
@@ -1,4 +1,4 @@
 class BulletinSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at, :url, :bulletin_type, :user_id
-  attribute :vote_ids
+  attribute :vote_ids, key: :votes
 end
