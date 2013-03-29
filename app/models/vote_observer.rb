7d6738d42cbbf000b2562ee7cb7dbcb2e4c29db0
mehulkar <mehul.kar@gmail.com>
2013-03-28 19:22:14 -0700

    bad typo

--- app/models/vote_observer.rb
+++ app/models/vote_observer.rb
@@ -6,7 +6,7 @@ class VoteObserver < ActiveRecord::Observer
     if bulletin
       # every 5th vote we'll tweet the bulletin and the url
       # TODO use bit.ly api to do short urls
-      if bulletin.votes.count > 1 && bulletin.votes.count.length % 5 == 0
+      if bulletin.votes.count > 1 && bulletin.votes.count % 5 == 0
         Twitter.update(bulletin.title + " " + bulletin.bulletin_url )
       end
     end
