f2d742b0867f7702c23f9f2e88c6c39f47a4bbee
mehulkar <mehul.kar@gmail.com>
2013-04-03 16:28:50 -0700

    Put twitter update in a rescue block

--- app/models/vote_observer.rb
+++ app/models/vote_observer.rb
@@ -7,7 +7,11 @@ class VoteObserver < ActiveRecord::Observer
       # every 5th vote we'll tweet the bulletin and the url
       # TODO use bit.ly api to do short urls
       if bulletin.votes.count > 1 && bulletin.votes.count % 5 == 0
-        Twitter.update(bulletin.title + " " + bulletin.bulletin_url )
+        begin
+          Twitter.update(bulletin.title + " " + bulletin.bulletin_url )
+        else
+          puts "Can't tweet this #{vote}"
+        end
       end
     end
   end
