f0446b395100093a70d2587869424dbdf0e5cd37
rakm <rakm@rakm.io>
2013-04-03 21:15:44 -0700

    rescue twitter update error

--- app/models/vote_observer.rb
+++ app/models/vote_observer.rb
@@ -2,14 +2,13 @@ class VoteObserver < ActiveRecord::Observer
 
   def after_create(vote)
     bulletin = vote.votable if vote.votable_type = "Bulletin"
-
     if bulletin
       # every 5th vote we'll tweet the bulletin and the url
       # TODO use bit.ly api to do short urls
       if bulletin.votes.count > 1 && bulletin.votes.count % 5 == 0
         begin
           Twitter.update(bulletin.title + " " + bulletin.bulletin_url )
-        else
+        rescue
           puts "Can't tweet this #{vote}"
         end
       end
