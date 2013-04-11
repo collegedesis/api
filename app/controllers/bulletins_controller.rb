ecb429ea41a3cbb31df5bd7c4b7bb0e3906969d1
rakm <rakm@rakm.io>
2013-04-11 11:41:21 -0700

    automatically vote on a bulletin when user creates it. Fixes #41

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -15,6 +15,13 @@ class BulletinsController < ApplicationController
   def create
     @bulletin = Bulletin.create(params[:bulletin])
     @bulletin.update_attributes(user_id: current_user.id)
+
+    # vote on it
+    @bulletin.votes.create(user_id: current_user.id)
+
+    session[:votedBulletinIds] = [] if !session[:votedBulletinIds]
+    session[:votedBulletinIds].push(@bulletin.id) if !session[:votedBulletinIds].include? @bulletin.id
+
     render json: @bulletin
   end
 
