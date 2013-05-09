7148626071971a761ef8a42521b7a1427bf32c68
mehulkar <mehul.kar@gmail.com>
2013-05-09 16:07:06 -0700

    Log twitter failure

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -106,8 +106,8 @@ class Bulletin < ActiveRecord::Base
     url = Rails.env.production? ? shortened_url : bulletin_url
     begin
       Twitter.update "#{self.title} #{url}"
-    rescue
-      puts "Tweet send failed"
+    rescue => e
+      puts "Twitter update failed: #{e.inspect}"
     end
   end
 
