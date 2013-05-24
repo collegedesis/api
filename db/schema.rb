21f30ce662c4850763386eb76a8e8860b71733b0
mehulkar <mehul.kar@gmail.com>
2013-05-23 22:40:47 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20130523164519) do
     t.integer  "bulletin_type", :default => 1
     t.string   "url"
     t.integer  "user_id"
+    t.boolean  "protected",     :default => false
     t.string   "slug"
     t.boolean  "is_dead",       :default => false
   end
@@ -53,12 +54,13 @@ ActiveRecord::Schema.define(:version => 20130523164519) do
   end
 
   create_table "messages", :force => true do |t|
-    t.string   "from_email"
-    t.string   "from_name"
     t.string   "subject"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.boolean  "test",       :default => true
+    t.string   "from_name"
+    t.string   "from_email"
   end
 
   create_table "notifications", :force => true do |t|
@@ -74,12 +76,12 @@ ActiveRecord::Schema.define(:version => 20130523164519) do
     t.integer  "int_ref"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
     t.string   "website"
-    t.string   "email"
     t.string   "facebook"
     t.string   "twitter"
     t.string   "youtube"
@@ -87,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20130523164519) do
     t.integer  "organization_type_id"
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
+    t.string   "email"
     t.integer  "university_id"
     t.boolean  "exposed",                  :default => true
     t.string   "slug"
@@ -130,6 +133,8 @@ ActiveRecord::Schema.define(:version => 20130523164519) do
     t.string   "password_hash"
     t.string   "full_name"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
   create_table "votes", :force => true do |t|
     t.integer  "votable_id"
