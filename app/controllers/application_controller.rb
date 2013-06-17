045c4a41da31a128aab25d546cdecdc958f81216
mehulkar <mehul.kar@gmail.com>
2013-06-16 23:37:56 -0700

    set access headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :set_access_control_headers
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
   end
 end
