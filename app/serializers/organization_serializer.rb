2cb18d92a41a77af03822e1f6e1cbababda85cd2
rakm <rakm@rakm.io>
2013-06-21 17:36:38 -0700

    Add social links to organization pages

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,5 +1,5 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :website, :display_name, :location, :slug, :university_name, :about, :reputation
+  attributes :id, :name, :website, :display_name, :location, :slug, :university_name, :about, :reputation, :twitter, :facebook
 
   # Association
   attribute :organization_type_id, key: :org_type_id
