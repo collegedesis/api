25a8338d8fc10c79cae5c55cb75b2eef570b53fa
rakm <rakm@rakm.io>
2013-07-04 17:49:44 -0700

    clean up membership specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,48 +1,53 @@
 require 'spec_helper'
 
 describe Membership do
-  it "should approve memberships if the organization auto approves memberships" do
-    org = FactoryGirl.create(:organization, :auto_approve_memberships => true)
-    user = FactoryGirl.create(:user)
-    mem = org.memberships.create(user_id: user.id)
-    mem.approved.should eq true
-  end
 
-  it "should not approve memberships if the organization does not auto approve membership" do
-    org = FactoryGirl.create(:organization, :auto_approve_memberships => false)
-    user = FactoryGirl.create(:user)
-    mem = org.memberships.create(user_id: user.id)
-    mem.approved.should eq false
+  describe "#new" do
+    let(:user) { FactoryGirl.create(:user) }
+    it "should be approved if the organization auto approves memberships" do
+      org = FactoryGirl.create(:organization, :auto_approve_memberships => true)
+      mem = org.memberships.create(user_id: user.id)
+      mem.approved.should eq true
+    end
+    it "should not approve memberships if the organization does not auto approve membership" do
+      org = FactoryGirl.create(:organization, :auto_approve_memberships => false)
+      mem = org.memberships.create(user_id: user.id)
+      mem.approved.should eq false
+    end
   end
 
   describe "Uniquness validations for memberships" do
     before(:each) do
-      @org1 = FactoryGirl.create(:organization, email: "something@something.com")
+      @org1 = FactoryGirl.create(:organization)
       @org2 = FactoryGirl.create(:organization)
-      @user1 = FactoryGirl.create(:user, email: "blah@blah.com")
+      @user1 = FactoryGirl.create(:user)
       @user2 = FactoryGirl.create(:user)
     end
-    it "should not create duplicate memberships" do
-      mem1 = Membership.create(user_id: @user1.id, organization_id: @org1.id)
-      mem2 = Membership.create(user_id: @user1.id, organization_id: @org1.id)
-      Membership.count.should eq 1
-    end
-    it "should be able to create multiple memberships for the same user" do
-      mem1 = Membership.create(user_id: @user1.id, organization_id: @org1.id)
-      mem2 = Membership.create(user_id: @user1.id, organization_id: @org2.id)
-      Membership.count.should eq 2
+
+    it "does not create duplicate memberships" do
+      Membership.create(user_id: @user1.id, organization_id: @org1.id)
+      mem2 = Membership.new(user_id: @user1.id, organization_id: @org1.id)
+      expect(mem2.valid?).to eq false
+      expect(mem2.errors.messages.keys.include?(:user_id)).to eq true
     end
 
-    it "should be able to create multiple memberships for one organization" do
-      mem1 = Membership.create(user_id: @user1.id, organization_id: @org1.id)
-      mem2 = Membership.create(user_id: @user2.id, organization_id: @org1.id)
-      Membership.count.should eq 2
+    it "can create multiple memberships for the same user" do
+      Membership.create(user_id: @user1.id, organization_id: @org1.id)
+      mem2 = Membership.new(user_id: @user1.id, organization_id: @org2.id)
+      expect(mem2.valid?).to eq true
+    end
+
+    it "can create multiple memberships for the same organization" do
+      Membership.create(user_id: @user1.id, organization_id: @org1.id)
+      mem2 = Membership.new(user_id: @user2.id, organization_id: @org1.id)
+      expect(mem2.valid?).to eq true
     end
   end
-  describe "#reject" do
-    let(:org) { FactoryGirl.create(:organization) }
-    let(:user) { FactoryGirl.create(:user) }
-    let(:membership) { org.memberships.create(user_id: user.id) }
+
+  describe "#destroy" do
+    let(:org)         { FactoryGirl.create(:organization) }
+    let(:user)        { FactoryGirl.create(:user) }
+    let(:membership)  { org.memberships.create(user_id: user.id) }
 
     it "sends an email to the user" do
       mailer = mock(MemberMailer)
