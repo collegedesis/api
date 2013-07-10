9f3a6003afa33400e75934bfed6c6361fa348f2f
rakm <rakm@rakm.io>
2013-07-10 15:32:43 -0700

    use mandrill

--- config/initializers/mailgun.rb
+++
@@ -1,9 +0,0 @@
-# Mailgun settings
-ActionMailer::Base.smtp_settings = {
-  :authentication           => :plain,
-  :address                  => "smtp.mailgun.org",
-  :port                     => 587,
-  :domain                   => ENV['MAILGUN_DOMAIN'],
-  :user_name                => ENV['MAILGUN_USER'],
-  :password                 => ENV['MAILGUN_PASSWORD']
-}
