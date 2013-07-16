a294f828edb9f23dbf7a8217bcf7e57b2d0f2a86
rakm <rakm@rakm.io>
2013-07-16 16:07:12 -0700

    text team when new user signs up

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,22 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     MemberMailer.welcome_email(user).deliver
+    begin
+      text_team(user)
+    end
+  end
+
+  def text_team(user)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+    if account_sid && auth_token
+      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+      @twilio_client.account.sms.messages.create(
+        :from => "+16418146200",
+        :to => "+19712642651",
+        :body => "Woot! #{user.full_name} just signed up!"
+      )
+    end
   end
 end
