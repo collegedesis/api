1068cc68f72ebf4b82e8b02f8f2b254d63a82fc5
rakm <rakm@rakm.io>
2013-07-17 10:24:52 -0700

    more frequent tweeting updating

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,6 +8,6 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(1.hours, 'Update popularity score') { Bulletin.all.each(&:update_score) }
-every(4.hours, 'Tweeting top 3 bulletins') { BulletinTweeter.tweet_top(3) }
+every(30.minutes, 'Update popularity score') { Bulletin.all.each(&:update_score) }
+every(1.hour, 'Tweeting top 3 bulletins') { BulletinTweeter.tweet_top(3) }
 every(1.day, 'expire bulletins', at: '23:59') { Bulletin.alive.each(&:expire) }
