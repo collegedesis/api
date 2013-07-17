a33ee7dbf8c53169cb8f2a4a61390a50c9f6d7c9
rakm <rakm@rakm.io>
2013-07-17 12:35:04 -0700

    serialize comments

--- app/serializers/bulletin_serializer.rb
+++ app/serializers/bulletin_serializer.rb
@@ -3,4 +3,5 @@ class BulletinSerializer < ActiveModel::Serializer
   attributes :bulletin_type, :slug, :author_id, :author_type
   attribute :url_to_serialize, key: :url
   attribute :score
+  attribute :comment_ids
 end
