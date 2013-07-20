971ff3bcadbcc9a12585c04f8d5f16c1d4bf1e94
mehulkar <mehul.kar@gmail.com>
2013-07-20 10:52:31 -0700

    tweet every 2 hours

--- lib/clock.rb
+++ lib/clock.rb
@@ -9,5 +9,5 @@ handler do |job|
 end
 
 every(30.minutes, 'Update popularity score') { Bulletin.all.each(&:update_score) }
-every(1.hour, 'Tweeting top 3 bulletins') { BulletinTweeter.tweet_top(3) }
+every(2.hour, 'Tweeting top 3 bulletins') { BulletinTweeter.tweet_top(3) }
 every(1.day, 'expire bulletins', at: '23:59') { Bulletin.alive.each(&:expire) }
