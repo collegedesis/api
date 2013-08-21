81faf253145a0696fb46c155a0ef635f5ed21e02
rakm <rakm@rakm.io>
2013-08-20 20:15:59 -0700

    remove bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,16 +36,5 @@ Collegedesis::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
- 
-  config.after_initialize do
-      Bullet.enable = true
-      Bullet.bullet_logger = true
-      Bullet.console = true
-      Bullet.growl = true
-      Bullet.rails_logger = true
-      Bullet.airbrake = false
-      Bullet.n_plus_one_query_enable = false
-      Bullet.unused_eager_loading_enable = false
-      Bullet.counter_cache_enable = false
-  end
+
 end
