7767d54fd40bb96a9d7bc68576bd496e3c786342
mehulkar <mehul.kar@gmail.com>
2013-08-20 23:56:26 -0700

    Serialize url

--- app/serializers/bulletin_serializer.rb
+++ app/serializers/bulletin_serializer.rb
@@ -1,7 +1,6 @@
 class BulletinSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at
-  attributes :bulletin_type, :slug, :author_id, :author_type
-  attribute :url_to_serialize, key: :url
+  attributes :bulletin_type, :slug, :author_id, :author_type, :url
   attribute :score
   attribute :comment_ids
 end
