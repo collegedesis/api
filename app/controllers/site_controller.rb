e5f443ff1a807c688e64b33ad8c608553e626d59
rakm <rakm@rakm.io>
2013-09-07 00:01:31 -0700

    remove authentication that we are not using

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,14 +1,4 @@
 class SiteController < ApplicationController
-  # before_filter :require_login, only: [:home]
-
   def home
-  end
-
-  private
-  def require_login
-    authenticate_or_request_with_http_basic('Administration') do |username, password|
-      md5_of_password = Digest::MD5.hexdigest(password)
-      username == "desi" && md5_of_password == 'b2bc70904b79e2c58089f88df5670b1b'
-    end
   end
 end
