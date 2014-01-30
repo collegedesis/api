13bcb2c8f38c39372ee94e9eec446ee032b980e7
mehulkar <mehul.kar@gmail.com>
2014-01-30 09:05:07 +0530

    generate new access token for every post

--- app/models/bulletin_facebooker.rb
+++ app/models/bulletin_facebooker.rb
@@ -1,9 +1,10 @@
 class BulletinFacebookPoster
+  require 'cgi'
   attr_accessor :bulletin, :user_graph_api_access_token
 
   def initialize(bulletin)
     @bulletin = bulletin
-    user_graph_api_access_token = USER_GRAPH_ACCESS_TOKEN
+    user_graph_api_access_token = get_access_token
     user_graph = Koala::Facebook::API.new(user_graph_api_access_token)
     accounts = user_graph.get_connections("me", "accounts")
     page_graph_api_access_token = accounts.first["access_token"] # TODO explicitly get the CollegeDesis page
@@ -19,4 +20,19 @@ class BulletinFacebookPoster
     @page.put_connections("me", 'feed', :message => @bulletin.title, :link => @bulletin.shortened_url)
   end
 
+  def get_access_token
+    open_graph_url = 'https://graph.facebook.com/oauth/access_token?'
+
+    params = {
+      client_id: ENV['FB_CLIENT_ID'],
+      client_secret: ENV['FB_CLIENT_SECRET'],,
+      redirect_uri: CGI.escape("https://collegedesis.com"),
+      grant_type: "client_credentials",
+    }
+    param_string = params.map {|k,v| "#{k}=#{v}"}.join('&')
+
+    request_url = open_graph_url + param_string
+    response = open(request_url)
+    return response.read.split('=').last
+  end
 end
