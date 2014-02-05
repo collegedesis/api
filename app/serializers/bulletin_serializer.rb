cb06b6b7c366f9e70906049d98839fc71d2928e8
mehulkar <mehul.kar@gmail.com>
2014-02-05 13:06:13 +0530

    Show views count on bulletins

--- app/serializers/bulletin_serializer.rb
+++ app/serializers/bulletin_serializer.rb
@@ -1,6 +1,6 @@
 class BulletinSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at
-  attributes :bulletin_type, :slug, :author_id, :author_type, :url
+  attributes :bulletin_type, :slug, :author_id, :author_type, :url, :views_count
   attribute :score
   attribute :comment_ids
 end
