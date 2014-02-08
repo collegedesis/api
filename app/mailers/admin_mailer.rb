3ccd9efeed6425f09c463ec2c0b5695d9fa2dcbb
mehulkar <mehul.kar@gmail.com>
2014-02-08 15:32:42 -0800

    include email address to CD admin when notifying of new registration

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,10 @@
 class AdminMailer < ActionMailer::Base
   default from: "CollegeDesis <brownkids@collegedesis.com>"
   def notify(membership)
-    mail(to: "notify@collegedesis.com", subject: "#{membership.user.full_name} registered via #{membership.organization.display_name}" )
+    mail(
+      to: "notify@collegedesis.com",
+      subject: "#{membership.user.full_name} registered via #{membership.organization.display_name}",
+      body: "Using email address: #{membership.user.email}"
+    )
   end
 end
