86648406b56def2401a8db6bb83fd09c067da3ff
mehulkar <mehul.kar@gmail.com>
2014-02-08 20:18:36 -0800

    fix facebook posting

--- app/models/bulletin_facebooker.rb
+++ app/models/bulletin_facebooker.rb
@@ -1,20 +1,21 @@
 class BulletinFacebookPoster
-  require 'cgi'
-  require 'open-uri'
-
   attr_accessor :bulletin, :user_graph_api_access_token
 
   def initialize(bulletin)
     @bulletin = bulletin
-    @page = Koala::Facebook::API.new(ENV['FB_PAGE_ACCESS_TOKEN'])
+    user_graph_api_access_token = USER_GRAPH_ACCESS_TOKEN
+    user_graph = Koala::Facebook::API.new(user_graph_api_access_token)
+    accounts = page_graph.get_connections("me", "accounts")
+    @page_graph_api_access_token = accounts.first["access_token"] # TODO explicitly get the CollegeDesis page
   end
 
   def self.post_top_bulletin
     bulletin_to_post = Bulletin.homepage.first
-    bulletin_to_post.post_to_facebook
+    bulletin_to_post.post
   end
 
   def post
-    @page.put_connections("me", 'feed', :message => @bulletin.title, :link => @bulletin.shortened_url)
+    @page_graph_api_access_token.put_connections("me", 'feed', message: @bulletin.title, link: @bulletin.shortened_url)
   end
+
 end
