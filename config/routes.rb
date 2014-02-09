cdcc19dc94feaec778c9568d4f5626868ad970e7
mehulkar <mehul.kar@gmail.com>
2014-02-08 21:03:32 -0800

    move routes outside version namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,27 @@
 Collegedesis::Application.routes.draw do
+  resources :api_keys, except: [:new, :edit]
   root to: 'site#home'
 
-  # Sessions
+  resources :organizations,           only: [:index, :show, :create, :update] do
+    collection { get :search }
+  end
+  resources :organization_types,      only: [:index, :show]
+  resources :users
+  resources :memberships,             only: [:index, :show, :destroy]
+  resources :membership_applications, only: [:index, :create, :show]
+  resources :membership_types,        only: [:index, :show]
+  resources :universities,            only: [:index, :show]
+  resources :bulletins,               only: [:index, :show, :create]
+  resources :comments,                only: [:index, :create]
+  resources :views,                   only: [:create]
   resources :sessions,                only: [:create, :destroy]
 
-  namespace :api do
-    namespace :v1 do
-      resources :organizations,           only: [:index, :show, :create, :update]
-      resources :organization_types,      only: [:index, :show]
-      resources :users
-      resources :memberships,             only: [:index, :show, :destroy]
-      resources :membership_applications, only: [:index, :create, :show]
-      resources :membership_types,        only: [:index, :show]
-      resources :universities,            only: [:index, :show]
-      resources :bulletins,               only: [:index, :show, :create]
-      resources :comments,                only: [:index, :create]
-      resources :votes,                   only: [:index, :create]
-      resources :views,                   only: [:create]
-      # TODO do this better
-      match 'info', to: 'stats#info'
+  # TODO do this better
+  get 'info', to: 'stats#info'
 
-      # Legacy. TODO needs update
-      get '/application/:code/approve', to: 'membership_applications#approve'
-      get '/application/:code/reject', to: 'membership_applications#reject'
-    end
-  end
+  # Legacy. TODO needs update
+  get '/application/:code/approve', to: 'membership_applications#approve'
+  get '/application/:code/reject', to: 'membership_applications#reject'
 
-  # Used to render inside the `noscript` tags for SEO
-  resources :d, only: [:show]
-  resources :n, only: [:show]
-
-  # Catch all to serve ember app
-  get '*foo', to: 'site#home'
+  match '/*path' => 'application#cors_preflight_check', via: :options
 end
