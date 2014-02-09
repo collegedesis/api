c926cadfcf0e9b7b37c12539dfbc042bb07957ff
mehulkar <mehul.kar@gmail.com>
2014-02-08 20:53:54 -0800

    don't try to text team

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,18 +2,4 @@ class UserObserver < ActiveRecord::Observer
   def after_create(user)
     MemberMailer.welcome_email(user).deliver
   end
-
-  def text_team(user)
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-
-    if account_sid && auth_token
-      @twilio_client = Twilio::REST::Client.new account_sid, auth_token
-      @twilio_client.account.sms.messages.create(
-        :from => "+16418146200",
-        :to => "+19712642651",
-        :body => "Woot! #{user.full_name} just signed up!"
-      )
-    end
-  end
 end
