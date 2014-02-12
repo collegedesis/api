5d80a529a0756ab87c2d1bf51c01a799a78f4815
mehulkar <mehul.kar@gmail.com>
2014-02-12 09:55:55 -0800

    remove after_filter for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  after_filter :log_log_in, only: [:create]
-
   def create
     user = User.where(email: params[:session][:email]).first
     if user && user.authenticate_merge_strategy(params[:session][:password])
@@ -16,15 +14,5 @@ class SessionsController < ApplicationController
     session = lookup_session(params[:session])
     session.expire
     render nothing: true, status: 204
-  end
-
-private
-  # TODO Use a real logger please. kthx
-  def log_log_in
-    if current_user
-      puts "----------#{params[:email]}logging in!---------"
-      puts current_user.email
-      puts "-----------logging in---------"
-    end
   end
 end
