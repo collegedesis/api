25e177441533f072575d308abcef4c574368f58f
mehulkar <mehul.kar@gmail.com>
2014-03-06 02:53:54 -0800

    comment out views table migration.
    Our production db is a clone of the old db which already has this table.
    It was added in a previous version. I manually deleted the entry of this
    migration from schema_migrations because there are migrations before
    this that have not been run. But since thist able already exits,
    this migration will not run. So I am commenting this out for now
    so that migrations will run and update the schema_migrations table
    and our schema.rb file. From now on, we should use schema:load instead
    of cloning the db, and then find some sane way to port over production
    data.

--- db/migrate/20140203222843_add_views_table_and_counter_cache_column.rb
+++ db/migrate/20140203222843_add_views_table_and_counter_cache_column.rb
@@ -1,18 +1,18 @@
 class AddViewsTableAndCounterCacheColumn < ActiveRecord::Migration
   def up
-    create_table :views do |t|
-      t.string   :ip
-      t.integer  :viewable_id
-      t.string   :viewable_type
+    # create_table :views do |t|
+    #   t.string   :ip
+    #   t.integer  :viewable_id
+    #   t.string   :viewable_type
+    #   t.timestamps
+    # end
 
-      t.timestamps
-    end
     # add counter cache column
-    add_column :bulletins, :views_count, :integer, default: 0
+    # add_column :bulletins, :views_count, :integer, default: 0
 
-    Bulletin.find(:all).each do |p|
-      Bulletin.update_counters p.id, :views_count => p.views.length
-    end
+    # Bulletin.find(:all).each do |p|
+    #   Bulletin.update_counters p.id, :views_count => p.views.length
+    # end
   end
 
   def down
