373baeae4d1db7c0e64a879a983938f5b6cec14d
mehulkar <mehul.kar@gmail.com>
2014-03-13 14:46:46 -0700

    Use auth_merge_strategy on new users in case someone signs up twice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,10 @@ class UsersController < ApplicationController
         return
       end
     end
-    if @user.authenticate(params[:user][:password])
+    if @user.authenticate_merge_strategy(params[:user][:password])
       render json: @user.session_api_key, status: 201
     else
-      render json: @user.errors.messages, status: 422
+      render json: "Error: User exists, but authentication failed", status: 401
     end
   end
 end
