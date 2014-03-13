73ba817724a974c04573f2eb3acfc15cf21bcafb
mehulkar <mehul.kar@gmail.com>
2014-03-12 20:24:25 -0700

    Don't try to log user.full_name if user doesn't exist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,17 @@
 class SessionsController < ApplicationController
   before_action :log_this, only: [:create]
+
   def create
     user = User.where(email: params[:session][:email]).first
     if user && user.authenticate_merge_strategy(params[:session][:password])
       puts "SUCCESS: #{user.full_name} was able to authenticate"
       render json: user.session_api_key, status: 201
     else
-      puts "FAIL: #{user.full_name} was not able to authenticate"
+      if user
+        puts "FAIL: #{user.full_name} was not able to authenticate"
+      else
+        puts "FAIL: #{params[:session][:email]} does not have an account."
+      end
       render json: {}, status: 401
     end
   end
